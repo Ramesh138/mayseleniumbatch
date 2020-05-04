d7a56a24dcb16368451ced035ecf679046869189
Ramesh138 <ponneriramesh158@gmail.com>
2020-05-04 23:14:05 +0530

    04/05/2020

---
+++ src/com/seleniumproject1/datatypes/Datatypes.java
@@ -0,0 +1,48 @@
+package com.seleniumproject1.datatypes;
+
+public class Datatypes {
+	
+	// Datatype is a character  or type of a variable or identifier 
+	
+	//Datatype is two types primitive  and non primitive datatype
+	
+	//Primitive : byte short int long === Numeric    float double ===Float    char == Character   boolean =====Boolean
+	
+	//Non primitive : String and Arrays and etc ...final static finally
+	
+	
+		byte byt=100;  // 8 bits  2 power 8 ============0
+		
+		short sho=12333; //  16 bits  2 power 16 =========== 0
+		
+		int in=12531 ; //  32 bits =========0
+		
+		long lo=11111112222222222L; //64 bits ============0l
+		
+		
+	//	byte >> short >> int >>long 
+		
+		float flo=25555.0000f;  //32 bits  ====0.0f
+		
+		double dou=25555555555555555555.0000d; // 64 bits====0.0d
+		
+		
+	//	float >double
+		
+		
+		char gender='F';  //=== '/u000'
+		
+		boolean flag=true;   /// false
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+	
+
+}
